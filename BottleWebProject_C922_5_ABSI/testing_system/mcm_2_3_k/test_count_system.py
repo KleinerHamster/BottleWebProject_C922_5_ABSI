0dd413cfd43643c2495dd00bf6894505bb920349
Камилла Балаева <kamilla-balaeva@mail.ru>
2022-05-19 18:45:42 +0300

    test coatings added  for fault-free calculation method

--- BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_count_system.py
+++ BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_count_system.py
@@ -4,7 +4,12 @@ import mcm_2_3_k
 class Test_test_Count(unittest.TestCase):
     def test_system_work_count(self):
         self.assertEqual(mcm_2_3_k.system_work_count(0.8,0.9, 0.7,0.75,0.8,80) ,74 )
-
+    def test_system_work_count_1(self):
+        self.assertEqual(mcm_2_3_k.system_work_count(0,0, 0,0,0,80) ,0 )
+    def test_system_work_count_2(self):
+        self.assertEqual(mcm_2_3_k.system_work_count(1,1, 1,1,1,80) ,80 )
+    def test_system_work_count_3(self):
+        self.assertEqual(mcm_2_3_k.system_work_count(0.28,0.01, 0.01,0.01,1,80) ,21 )
 if __name__ == '__main__':
     unittest.main()
 
