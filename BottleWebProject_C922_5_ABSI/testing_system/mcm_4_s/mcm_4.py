948845bba0a9a53f93e09b669e3dba0b1f349fb4
sofiy <sofiy@LAPTOP-LUNGFFEP>
2022-05-19 17:33:03 +0300

    create file for unittest

--- BottleWebProject_C922_5_ABSI/testing_system/mcm_4_s/mcm_4.py
+++ BottleWebProject_C922_5_ABSI/testing_system/mcm_4_s/mcm_4.py
@@ -1,1 +1,32 @@
+def moment_of_receipt_of_the_application(random_number, a, t1):
+    import math
+    convert_number_to_ln=-math.log(random_number)
+    ri=(1/a)*convert_number_to_ln
+    ti=round(t1+ri,3)
+    return ti
 
+
+def link1(list_link, ti, t1):
+    for i in range(len(list_link)):
+        if ti>list_link[i]:
+           list_link[i]=round(ti+t1,3) 
+           break
+    
+    return list_link
+
+
+def count_of_link(list_link):
+    for link in list_link:
+        if link:
+            return True
+        else:
+            return False
+
+
+def check_result(list_result):
+    a=0                                                                                                                                                                                                                                                                 
+    for result in list_result:
+        a+=result
+    a/=len(list_result)
+
+    return a
--- BottleWebProject_C922_5_ABSI/testing_system/mcm_4_s/test_smth.py
+++ BottleWebProject_C922_5_ABSI/testing_system/mcm_4_s/test_smth.py
@@ -1,1 +1,46 @@
+import unittest
+import mcm_4
+
+class TestUM(unittest.TestCase):
+
+    def test_A(self):
+        self.assertEqual(mcm_4.moment_of_receipt_of_the_application(0.32,32,23), 23.036)
+
+    # нужно ли делать с числами которых у нас заведомо не будет (например, -1?)
+    def test_B(self):
+        self.assertEqual(mcm_4.moment_of_receipt_of_the_application(0.24, 0.42, 2494), 2497.398)
+
+    def test_C(self):
+        self.assertEqual(mcm_4.moment_of_receipt_of_the_application(3289, 8429.32, -0.323), -0.324)
+
+
+    # следующий тест
+    def test_a2(self):
+        self.assertEqual(mcm_4.link1([23.39,24.90,25.42,86.45],23,42), [23.39,24.90,25.42,86.45])
+
+    def test_b2(self):
+        self.assertEqual(mcm_4.link1([43,245,53,25],89,38), [127,245,53,25])
+
+
+    # следующий тест
+    def test_a3(self):
+        self.assertEqual(mcm_4.count_of_link([23.39,24.90,25.42,86.45]), True)
+
+    def test_b3(self):
+        self.assertEqual(mcm_4.count_of_link(['','','','']), False)
+
+
+    # next test
+    def test_a4(self):
+        self.assertEqual(mcm_4.check_result([43,24,54,23]), 36)
+
+    def test_b4(self):
+        self.assertEqual(mcm_4.check_result([1,4,4]), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
 
