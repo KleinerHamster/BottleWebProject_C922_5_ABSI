10433afede212b7d25fc3a41f01ad752408a7481
Камилла Балаева <kamilla-balaeva@mail.ru>
2022-05-20 14:11:53 +0300

    final test coatings added

--- BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_count_system.py
+++ BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_count_system.py
@@ -10,14 +10,8 @@ class Test_test_Count(unittest.TestCase):
         self.assertEqual(mcm_2_3_k.system_work_count(1,1, 1,1,1,80) ,80 )
     def test_system_work_count_3(self):
         self.assertEqual(mcm_2_3_k.system_work_count(0.28,0.01, 0.01,0.01,1,80) ,21 )
+    def test_system_work_count_4(self):
+        self.assertEqual(mcm_2_3_k.system_work_count(0.38, 1, 0.59, 0.01,0.29, 80) ,58 )
 if __name__ == '__main__':
     unittest.main()
-
-
-         
-
-
-
-
-
 
--- BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_result.py
+++ BottleWebProject_C922_5_ABSI/testing_system/mcm_2_3_k/test_result.py
@@ -5,7 +5,15 @@ class Test_test_result(unittest.TestCase):
         
     def test_itog(self):
         self.assertEqual(mcm_2_3_k.function_itog(0.8,0.9, 0.7,0.75,0.8,80),0.0403)
-
+    def test_itog_2_2(self):
+        self.assertEqual(mcm_2_3_k.function_itog(0.28,0.01, 0.01,0.01,1,80),0.0247)
+    def test_itog_3(self):
+        self.assertEqual(mcm_2_3_k.function_itog(0,0,0,0,0,80),0)
+    def test_itog_4(self):
+        self.assertEqual(mcm_2_3_k.function_itog(1,1,1,1,1,80),0)
+    def test_itog_5(self):
+        self.assertEqual(mcm_2_3_k.function_itog(0.38,1, 0.59,0.01,0.29,80),0.0132)
+        
 if __name__ == '__main__':
     unittest.main()
 
